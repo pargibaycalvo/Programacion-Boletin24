c25db1aaf953c08240cc0be1f62fe5fe58f0ad82
ped90 <ped90@ped90-X550CA>
2017-04-20 17:33:18 +0200

    Boletin24

---
+++ src/boletin24/Boletin24.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin24;
+
+/**
+ *
+ * @author ped90
+ */
+public class Boletin24 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Ventana1 obv = new Ventana1();
+        obv.setVisible(true);
+        
+        Ventana2 obx = new Ventana2();
+        obx.setVisible(true);
+        
+        
+    }
+    
+}
